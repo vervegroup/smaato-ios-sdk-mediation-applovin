a56a7db1a26dcf63840f891436e7e39f860a83b5
ashwinee-smaato <amit.angarkar@smaato.com>
2023-10-26 15:35:24 +0200

    11.11.3.0

---
+++ SmaatoSDKAdapters/ApplovinWaterfall/SmaatoApplovinMediationAdapter.h
@@ -0,0 +1,13 @@
+//
+//  SmaatoApplovinMediationAdapter.h
+//  SmaatoSDKAdapters
+//
+//  Created by Ashwinee Mhaske on 05/10/23.
+//  Copyright © 2023 Smaato. All rights reserved.
+//
+
+#import <AppLovinSDK/AppLovinSDK.h>
+
+@interface SmaatoApplovinMediationAdapter:ALMediationAdapter<MAAdViewAdapter, MAInterstitialAdapter, MARewardedAdapter>
+
+@end
