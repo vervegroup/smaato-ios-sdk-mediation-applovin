e3a80799670e1ec0c2088ef126039af7ad34a48d
paresh-smaato <paresh.karnawat@smaato.com>
2025-01-21 08:51:56 +0100

    13.0.0.0

---
+++ SmaatoSDKAdapters/SmaatoApplovinMediationAdapter.h
@@ -0,0 +1,12 @@
+//
+//  SmaatoApplovinMediationAdapter.h
+//  SmaatoSDKAdapters
+//
+//  Copyright © 2023 Smaato. All rights reserved.
+//
+
+#import <AppLovinSDK/AppLovinSDK.h>
+
+@interface SmaatoApplovinMediationAdapter:ALMediationAdapter<MAAdViewAdapter, MAInterstitialAdapter, MARewardedAdapter>
+
+@end
